a6ddc16a0f59d0c318cc5e4a9c7e456237991b8f
mliu0515 <chonghualiu@utexas.edu>
2024-04-04 13:05:45 -0500

    modified file structure

---
+++ transcription_module.py
@@ -0,0 +1,44 @@
+from pyannote.audio import Pipeline
+from pyannote_whisper.pyannote_whisper.utils import diarize_text
+from openai import OpenAI
+import torch
+import whisper
+import json
+
+
+# load ../config.json in a dictionary
+audio_path = "pyannote_whisper/data/SIMPSON.mp3"
+keys_dict = json.load(open("config.json"))
+openai_key = keys_dict["api_key"]
+pyannote_key = keys_dict["use_auth_token"]
+# initialize OpenAI client with your API key
+
+client = OpenAI(api_key=openai_key)
+pipeline = Pipeline.from_pretrained("pyannote/speaker-diarization-3.1",
+                                    use_auth_token=pyannote_key)
+audio_file = open(audio_path, "rb")
+
+def whisper_and_diarization(audio_file):
+    asr_result = client.audio.transcriptions.create(
+    model="whisper-1", 
+    file=audio_file,
+    timestamp_granularities=["word"],
+    response_format="verbose_json"
+    )
+    diarization_result = pipeline(audio_path)
+    final_result = diarize_text(asr_result, diarization_result)
+
+    lines = []
+    for seg, spk, sent in final_result:
+        line = f'{seg.start:.2f} {seg.end:.2f} {spk} {sent}'
+        lines.append(line)
+    return lines
+
+if __name__ == "__main__":
+    lines = whisper_and_diarization(audio_file)
+    # write each line on seperate lines
+
+    with open("output.txt", "w") as f:
+        for line in lines:
+            f.write(line)
+            f.write("\n")
