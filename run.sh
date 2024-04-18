078d962e7436355ae7e2c668f566cbba2ab65540
CHENGYANG WU <cw38637@login1.ls6.tacc.utexas.edu>
2024-04-18 15:55:00 -0500

    add llama3 example

--- run.sh
+++ run.sh
@@ -2,14 +2,14 @@
 #SBATCH -J run           # Job name
 #SBATCH -o run.o%j       # Name of stdout output file
 #SBATCH -e run.e%j       # Name of stderr error file
-#SBATCH -p vm-small        # Queue (partition) name
+#SBATCH -p gpu-a100        # Queue (partition) name
 #SBATCH -N 1               # Total # of nodes (must be 1 for serial)
 #SBATCH -n 1               # Total # of mpi tasks (should be 1 for serial)
 #SBATCH -t 1:00:00        # Run time (hh:mm:ss)
-#SBATCH -A EAR23036        # Project/Allocation name (req'd if you have more than 1)
+#SBATCH -A CCR24006        # Project/Allocation name (req'd if you have more than 1)
 cd /work/07016/cw38637/ls6/nlp/
 
-# module load cuda/12.0
-source activate
+module load cuda/12.2
+conda activate nlp
 
-python blip2.py
+python llama3_example.py
