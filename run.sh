745f50a2d035ae8994662a05d3dd1bf2edb87b92
CHENGYANG WU <cw38637@login1.ls6.tacc.utexas.edu>
2024-04-19 15:58:30 -0500

    add torchrun example

--- run.sh
+++ run.sh
@@ -2,14 +2,17 @@
 #SBATCH -J run           # Job name
 #SBATCH -o run.o%j       # Name of stdout output file
 #SBATCH -e run.e%j       # Name of stderr error file
-#SBATCH -p gpu-a100        # Queue (partition) name
+#SBATCH -p gpu-a100-small        # Queue (partition) name
 #SBATCH -N 1               # Total # of nodes (must be 1 for serial)
 #SBATCH -n 1               # Total # of mpi tasks (should be 1 for serial)
-#SBATCH -t 1:00:00        # Run time (hh:mm:ss)
+#SBATCH -t 00:30:00        # Run time (hh:mm:ss)
 #SBATCH -A CCR24006        # Project/Allocation name (req'd if you have more than 1)
 cd /work/07016/cw38637/ls6/nlp/
 
 module load cuda/12.2
 conda activate nlp
 
-python llama3_example.py
+torchrun --nproc_per_node 1 example_chat_completion.py \
+    --ckpt_dir /work/07016/cw38637/ls6/llama3-weights/Meta-Llama-3-8B-Instruct/ \
+    --tokenizer_path /work/07016/cw38637/ls6/llama3-weights/Meta-Llama-3-8B-Instruct/tokenizer.model \
+    --max_seq_len 512 --max_batch_size 6
